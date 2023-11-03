aabae6cfff28c0dc520685b9dd024c52e6d1e5c8
Raquel Barbieri <107284346+raquelbarbieri@users.noreply.github.com>
2023-11-03 17:16:23 -0300

    Corrigindo erro no código

--- index.js
+++ index.js
@@ -12,18 +12,24 @@ let derrotas = prompt("Digite o seu número de derrotas")
 
 let resultado = (vitorias - derrotas)
 
-if(resultado =< 10) {
+if(resultado <= 10) {
   console.log("O herói de nome " + nickname + " tem saldo de " + resultado + " e está no nível de Ferro")
-} else if(resultado >= 11 && resultado <= 20){
+} 
+else if(resultado >= 11 && resultado <= 20){
   console.log("O herói de nome " + nickname + " tem saldo de " + resultado + " e está no nível de Bronze")
-} else if(resultado >= 21 && resultado <= 50){
+} 
+else if(resultado >= 21 && resultado <= 50){
   console.log("O herói de nome " + nickname + " tem saldo de " + resultado + " e está no nível de Prata")
-} else if(resultado >= 51 && resultado <= 80){
+} 
+else if(resultado >= 51 && resultado <= 80){
   console.log("O herói de nome " + nickname + " tem saldo de " + resultado + " e está no nível de Ouro")
-} else if(resultado >= 81 && resultado <= 90){
-    console.log("O herói de nome " + nickname + " tem saldo de " + resultado + " e está no nível de Diamante")
-} else if(resultado >= 91 && resultado <= 100){
-    console.log("O herói de nome " + nickname + " tem saldo de " + resultado + " e está no nível de Lendário")
-} else if(resultado >=101){
-    console.log("O herói de nome " + nickname + " tem saldo de " + resultado + " e está no nível de Imortal")
+} 
+else if(resultado >= 81 && resultado <= 90){
+  console.log("O herói de nome " + nickname + " tem saldo de " + resultado + " e está no nível de Diamante")
+} 
+else if(resultado >= 91 && resultado <= 100){
+  console.log("O herói de nome " + nickname + " tem saldo de " + resultado + " e está no nível de Lendário")
+} 
+else if(resultado >=101){
+  console.log("O herói de nome " + nickname + " tem saldo de " + resultado + " e está no nível de Imortal")
 }
