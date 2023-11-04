070cb585d294e6adda050696645db12c16fd0ff0
Raquel Barbieri <107284346+raquelbarbieri@users.noreply.github.com>
2023-11-04 11:00:24 -0300

    Update and rename index.js to index-v1.js

--- index-v1.js
+++ index-v1.js
@@ -1,35 +1,40 @@
-let nickname = prompt("Digite o seu nickname para começar a aventura do ranking");
 
-console.log("Olá, nickname. Seja bem-vindo!");
+let nickname = prompt("Digite o seu nickname para começar a aventura do ranking: ");
 
-console.log("Vamos começar?")
+console.log("Olá, " + nickname + " Seja bem-vindo!");
 
-let vitorias = prompt("Primeiro, digite o seu número de vitórias");
+console.log("Vamos começar?");
 
-console.log("Legal, agora digite o seu número de derrotas");
+let vitorias = prompt("Primeiro, digite o seu número de vitórias: ");
 
-let derrotas = prompt("Digite o seu número de derrotas")
+let derrotas = prompt("Agora, digite o seu número de derrotas: ");
 
-let resultado = (vitorias - derrotas)
+let resultado = (vitorias - derrotas);
 
-if(resultado <= 10) {
+if(resultado <= 10){
   console.log("O herói de nome " + nickname + " tem saldo de " + resultado + " e está no nível de Ferro")
-} 
+}
+
 else if(resultado >= 11 && resultado <= 20){
   console.log("O herói de nome " + nickname + " tem saldo de " + resultado + " e está no nível de Bronze")
-} 
+}
+
 else if(resultado >= 21 && resultado <= 50){
   console.log("O herói de nome " + nickname + " tem saldo de " + resultado + " e está no nível de Prata")
 } 
+
 else if(resultado >= 51 && resultado <= 80){
   console.log("O herói de nome " + nickname + " tem saldo de " + resultado + " e está no nível de Ouro")
 } 
+
 else if(resultado >= 81 && resultado <= 90){
   console.log("O herói de nome " + nickname + " tem saldo de " + resultado + " e está no nível de Diamante")
-} 
+}
+
 else if(resultado >= 91 && resultado <= 100){
   console.log("O herói de nome " + nickname + " tem saldo de " + resultado + " e está no nível de Lendário")
 } 
+  
 else if(resultado >=101){
   console.log("O herói de nome " + nickname + " tem saldo de " + resultado + " e está no nível de Imortal")
 }
